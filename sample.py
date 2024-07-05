9c92fa229508432cdb40ab783fb0f4b027d9afbc
Athul A <athulkannan0123@gmail.com>
2024-07-05 15:28:09 +0530

    sample_v3

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 # Path to ChromeDriver
 service = Service('/usr/local/bin/chromedriver')
