2331d3f7f22a3d34c1e04c24c5ec5cd5bef2f0ea
Athul A <athulkannan0123@gmail.com>
2024-07-05 15:09:55 +0530

    added testing code

---
+++ sample.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+# Path to ChromeDriver
+service = Service('/usr/local/bin/chromedriver')
+
+options = Options()
+options.binary_location = '/usr/bin/google-chrome'  # Path to Chrome binary
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')  # If you want to run Chrome in headless mode
+
+driver = webdriver.Chrome(service=service, options=options)
+driver.get('https://www.google.com')
+print(driver.title)
+driver.quit()
